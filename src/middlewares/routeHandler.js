8c4d085bb59d07ec5b85cf2fd970294277a0c6a1
Kaian Samuel <kaiansamuelster@gmail.com>
2024-12-01 13:26:04 -0300

    Obtendo parametros

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -2,10 +2,16 @@ import { routes } from '../routes.js'
 
 export function routeHandler(req, res) {
     const route = routes.find((route) => {
-        return route.method === req.method && route.path === req.url
+        return route.method === req.method && route.path.test(req.url)
     })
 
     if (route) {
+        const routParams = req.url.match(route.path)
+
+        const { ...params } = routParams.groups
+
+        req.params = params
+        
         return route.controller(req, res)
     }
 
---
+++ src/utils/parseRoutePath.js
@@ -0,0 +1,9 @@
+export function parseRoutePath(path) {
+    const routeParamametersRegex = /:([a-zA-Z]+)/g
+
+    const withParams = path.replaceAll(routeParamametersRegex, "(?<$1>[a-x0-9-_]+)")
+
+    const pathRegex = new RegExp(params)
+
+    return pathRegex
+}
