eda5f331b683cc755689776f6bd29643f1022a88
Kaian Samuel <kaiansamuelster@gmail.com>
2024-12-01 21:54:17 -0300

    Escrevendo e lendo dados no arquivo

--- database.js
+++ database.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises'
+
+const DATABASE_PATH = new URL('db.json', import.meta.url)
 export class Database {
   database = {}
+
+  constructor() {
+    fs.readFile(DATABASE_PATH, 'utf-8')
+    .then((data) => {
+      this.database = JSON.parse(data)
+    }).catch(() => this.persist())
+    this.persist()
+  }
+
+  persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+  }
 
   insert(table, data) {
     if(Array.isArray(this.database[table])) {
@@ -7,6 +22,8 @@ export class Database {
     } else {
       this.database[table] = [data]
     }
+
+    this.persist()
   } 
 
   select(table) {
